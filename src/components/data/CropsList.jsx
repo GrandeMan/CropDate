168e0cbf4b74aba85e71e01f4d1c1405f9186f7c
Joshua Morales <114616062+GrandeMan@users.noreply.github.com>
2023-11-25 16:35:15 +0000

    Fix price display error

--- src/components/data/CropsList.jsx
+++ src/components/data/CropsList.jsx
@@ -24,7 +24,7 @@ const CropsList = () => {
 						"https://agrimarketwatch.herokuapp.com/crops/daily/recent"
 					);
 					setCropsData(response.data);
-					// console.log(response.data);
+					console.log(response.data);
 					localStorageData("cropsData", response.data);
 					setLoading(false);
 				}
@@ -51,96 +51,8 @@ const CropsList = () => {
 					</li>
 				))}
 			</ul>
-			{/* <ul>
-				{tempData.map((crop, index) => (
-					<li key={index}>
-						<h2 className="text-3xl font-bold underline">
-							{crop.category}
-						</h2>
-						<p>{crop.commodity}</p>
-						{crop.date}
-						<CurrencyFormatter value={crop.price} />
-						{crop.unit} {crop.volume}
-					</li>
-				))}
-			</ul> */}
 		</div>
 	);
 };
 
 export default CropsList;
-
-// let tempData = [
-// 	{
-// 		category: "root crops",
-// 		commodity: "yam (local)",
-// 		date: "2023-11-20 00:00:00",
-// 		price: 0.0,
-// 		unit: "kg",
-// 		volume: 0.0,
-// 	},
-// 	{
-// 		category: "root crops",
-// 		commodity: "yam (imported)",
-// 		date: "2023-11-20 00:00:00",
-// 		price: 0.0,
-// 		unit: "kg",
-// 		volume: 0.0,
-// 	},
-// 	{
-// 		category: "root crops",
-// 		commodity: "dasheen(local)",
-// 		date: "2023-11-20 00:00:00",
-// 		price: 13.89,
-// 		unit: "kg",
-// 		volume: 756.0,
-// 	},
-// 	{
-// 		category: "root crops",
-// 		commodity: "dasheen(imported)",
-// 		date: "2023-11-20 00:00:00",
-// 		price: 6.67,
-// 		unit: "kg",
-// 		volume: 360.0,
-// 	},
-// 	{
-// 		category: "root crops",
-// 		commodity: "eddoe (local)",
-// 		date: "2023-11-20 00:00:00",
-// 		price: 0.0,
-// 		unit: "kg",
-// 		volume: 0.0,
-// 	},
-// 	{
-// 		category: "root crops",
-// 		commodity: "eddoe (imported)",
-// 		date: "2023-11-20 00:00:00",
-// 		price: 0.0,
-// 		unit: "kg",
-// 		volume: 0.0,
-// 	},
-// 	{
-// 		category: "condiments and spices",
-// 		commodity: "celery",
-// 		date: "2023-11-20 00:00:00",
-// 		price: 120.0,
-// 		unit: "bndl.",
-// 		volume: 150.0,
-// 	},
-// 	{
-// 		category: "condiments and spices",
-// 		commodity: "chive (l)",
-// 		date: "2023-11-20 00:00:00",
-// 		price: 50.0,
-// 		unit: "bndl.",
-// 		volume: 330.0,
-// 	},
-// 	{
-// 		category: "condiments and spices",
-// 		commodity: "thyme (s)",
-// 		date: "2023-11-20 00:00:00",
-// 		price: 80.0,
-// 		unit: "bndl.",
-// 		volume: 32.0,
-// 	},
-// ];
