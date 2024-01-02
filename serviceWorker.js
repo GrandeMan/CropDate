f027fc3ba0bb05545391aae904eef864a576a2cf
Grandeman <joshuajmorales01@gmail.com>
2024-01-02 19:27:56 -0400

    Update app.webmanifest and package.json, add service worker caching

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,1 +1,13 @@
-console.log("Hello from your Service Worker file!");
+const assets = [
+  "/dist/index.html",
+  "/dist/assets/*.css",
+  "/dist/assets/*.js",
+  "/dist/assets/*.map",
+  "sw-register.js",
+];
+
+self.addEventListener("install", (event) => {
+  caches.open("assets").then((cache) => {
+    cache.addAll(assets);
+  });
+});
